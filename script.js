a5f6dbda3172b00271b57fc09dd41754fb638044
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-20 13:11:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
-
+let element = document.getElementById('bands');
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let sortedSpots = touristSpots.sort((a,b)=>{
+	let a1 = ((a.replace("A ",'')).replace("An ", '')).replace("The ", '');
+	let b1 = ((b.replace("A ",'')).replace("An ", '')).replace("The ", '');
+	// console.log(a1,b1)
+	if(b1>a1)
+		return -1;
+	if(a1>b1)
+		return 1;
+	return 0;
+})
+// console.log(sortedSpots);
+let items = '';
+sortedSpots.forEach((item)=>{
+items += "<li>"+item+"</li>";
+	})
+element.innerHTML = items;
